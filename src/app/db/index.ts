6da96c9d5cb0cbba51ca45065d7c89fb93214cd3
ketexon <zane.a.s.clark@gmail.com>
2025-02-01 21:29:00 -0800

    added readystate to connection

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -5,10 +5,12 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 export async function connect(uri: string = MONGODB_URI) {
-	await mongoose.connect(
-		uri,
-		{
-			dbName: "jotter",
-		}
-	);
+	if(mongoose.connection.readyState !== 1) {
+		await mongoose.connect(
+			uri,
+			{
+				dbName: "jotter",
+			}
+		)
+	}
 }
